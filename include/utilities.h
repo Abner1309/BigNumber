38738b44a0dcc369317fbd7eedff4ab9906bb0c5
Abner1309 <soul_abner@hotmail.com>
2026-05-03 12:03:10 -0300

    FEAT: New Utilities Functions

--- include/utilities.h
+++ include/utilities.h
@@ -2,5 +2,7 @@
 #define BIGNUMBER_UTILITIES_H
 
 void memory_verification(const void* pointer);
+void file_verification(const void* pointer);
+void print_error();
 
 #endif //BIGNUMBER_UTILITIES_H
--- src/utilities.c
+++ src/utilities.c
@@ -8,3 +8,15 @@ void memory_verification(const void* pointer) {
         exit(EXIT_FAILURE);
     }
 }
+
+void file_verification(const void* pointer) {
+    if (pointer == NULL) {
+        fprintf(stderr, "Error: The File Could Not Be Open\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void print_error() {
+    fprintf(stderr, "Error\n");
+    exit(EXIT_FAILURE);
+}
