7d69dc81d36e02465234eda56975e6bca3efb910
Abner1309 <soul_abner@hotmail.com>
2026-05-04 14:33:17 -0300

    FEAT: Extraction Function and Verify First Digit Function

---
+++ include/receiving_data.h
@@ -0,0 +1,8 @@
+#ifndef BIGNUMBER_RECEIVING_DATA_H
+#define BIGNUMBER_RECEIVING_DATA_H
+#include "structure.h"
+
+void verify_first_digit(const Operation* operation, int number, char c);
+Operation* extraction();
+
+#endif //BIGNUMBER_RECEIVING_DATA_H
---
+++ src/receiving_data.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include "receiving_data.h"
+#include "utilities.h"
+
+void verify_first_digit(const Operation* operation, const int number, const char c) {
+    if (c == '+') {
+        if (number == 1) { operation->number1->signal = '+';}
+        else if (number == 2) { operation->number2->signal = '+'; }
+        else { print_error(); }
+    }
+    else if (c == '-') {
+        if (number == 1) { operation->number1->signal = '-';}
+        else if (number == 2) { operation->number2->signal = '-'; }
+        else { print_error(); }
+    }
+    else if (c >= '0' && c <= '9') {
+        if (number == 1) {
+            operation->number1->digits[0] = c;
+            operation->number1->elements++;
+            operation->number1->signal = '+';
+        }
+        else if (number == 2) {
+            operation->number2->digits[0] = c;
+            operation->number2->elements++;
+            operation->number2->signal = '+';
+        }
+        else { print_error(); }
+    }
+    else { print_error(); }
+}
+
+Operation* extraction() {
+    char input_file_path[512];
+
+    // INPUT FILE
+    printf("Enter the path to the input file: ");
+    fgets(input_file_path, sizeof(input_file_path), stdin);
+    input_file_path[strcspn(input_file_path, "\n")] = '\0';
+
+    // OPEN FILE
+    FILE* arq_input = fopen(input_file_path, "r");
+    file_verification(arq_input);
+
+    // CREATE POINTER FOR OPERATION
+    Operation* operation = memory_allocation();
+
+    // RECEIVE FIRST BIG NUMBER
+    int c = 0;
+    verify_first_digit(operation, 1, (char) fgetc(arq_input));
+    while ((c = fgetc(arq_input)) != EOF && c != '\n' && c != ' ') {
+        add_element(operation, 1, (char) c);
+    }
+
+    // RECEIVE SECOND BIG NUMBER
+    verify_first_digit(operation, 2, (char) fgetc(arq_input));
+    while ((c = fgetc(arq_input)) != EOF && c != '\n' && c != ' ') {
+        add_element(operation, 2, (char) c);
+    }
+
+    // RECEIVE OPERATOR
+    add_operator(operation, (char) fgetc(arq_input));
+
+    return operation;
+}
