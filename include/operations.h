3939c3225de3a932e133d5f294dd1064fc403ced
Abner1309 <soul_abner@hotmail.com>
2026-05-07 08:27:48 -0300

    FEAT: Sum Operation Implemented

---
+++ include/operations.h
@@ -0,0 +1,10 @@
+#ifndef BIGNUMBER_OPERATIONS_H
+#define BIGNUMBER_OPERATIONS_H
+#include "structure.h"
+
+Number* operation_sum(const Operation* operation);
+Number* operation_subtraction(Operation* operation);
+Number* operation_multiplication(Operation* operation);
+Number* operation_division(Operation* operation);
+
+#endif //BIGNUMBER_OPERATIONS_H
