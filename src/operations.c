7b25ff34ca7ade9a3a1fbb0abae1516585f65dac
Abner1309 <soul_abner@hotmail.com>
2026-05-11 20:53:55 -0300

    STYLE: change in the name of a variable

--- src/operations.c
+++ src/operations.c
@@ -61,28 +61,41 @@ Number* operation_subtraction(const Operation* operation) {
     long int a = operation->number1->elements - 1;
     long int b = operation->number2->elements - 1;
     long int c = result->capacity - 1;
-    int sum = 0;
+    int minus = 0;
     while (a > -1 && b > -1) {
-        sum = (operation->number1->digits[a] - '0') - (operation->number2->digits[b] - '0');
-        result->digits[c] = (char) (sum + '0');
+        minus = (operation->number1->digits[a] - '0') - (operation->number2->digits[b] - '0');
+        result->digits[c] = (char) (minus + '0');
         result->elements++;
         a--; b--; c--;
     }
     while (a > -1) {
-        sum = (operation->number1->digits[a] - '0');
-        result->digits[c] = (char) (sum + '0');
+        minus = (operation->number1->digits[a] - '0');
+        result->digits[c] = (char) (minus + '0');
         result->elements++;
         a--; c--;
     }
     while (b > -1) {
-        sum = (operation->number2->digits[b] - '0');
-        result->digits[c] = (char) (sum + '0');
+        minus = (operation->number2->digits[b] - '0');
+        result->digits[c] = (char) (minus + '0');
         result->elements++;
         b--; c--;
     }
 
     // SUBTRACTION OPERATION - PART II
     subtraction_helper(result);
+
+    return result;
+}
+
+Number* operation_multiplication(const Operation* operation) {
+    // DETERMINE CAPACITY
+    const long int capacity = operation->number1->elements + operation->number2->elements;
+
+    // MEMORY ALLOCATION
+    Number* result = memory_allocation_number(operation, capacity);
+
+    // MULTIPLICATION OPERATION
+
 
     return result;
 }
