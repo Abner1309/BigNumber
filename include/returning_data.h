9246ce7831848ade248578ef2dd3458dfd2ae45d
Abner1309 <soul_abner@hotmail.com>
2026-05-11 10:22:05 -0300

    FEAT: final_step.c implemented

---
+++ include/returning_data.h
@@ -0,0 +1,8 @@
+#ifndef BIGNUMBER_RETURNING_DATA_H
+#define BIGNUMBER_RETURNING_DATA_H
+#include "structure.h"
+
+void save_result(FILE* arq, const Number* number);
+void final_step(Operation* operation, Number* result);
+
+#endif //BIGNUMBER_RETURNING_DATA_H
---
+++ src/returning_data.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include "returning_data.h"
+#include "utilities.h"
+
+void save_result(FILE* arq, const Number* number) {
+    int i = 0;
+
+    // IGNORE LEADING ZEROS
+    while (i < number->capacity && number->digits[i] == '0') {
+        i++;
+    }
+
+    // IF ALL DIGITS ARE ZEROS
+    if (i == number->capacity) { fprintf(arq, "0"); }
+    // IF THERE IS AT LEAST ONE DIGIT OTHER THAN ZERO
+    else {
+        // PRINT SIGNAL
+        fprintf(arq, "%c", number->signal);
+        // PRINT DIGITS
+        const int j = i;
+        while (i < number->elements + j) {
+            fprintf(arq, "%c", number->digits[i]);
+            i++;
+        }
+    }
+
+    // PRINT SPACE
+    fprintf(arq, "\n");
+}
+
+void final_step(Operation* operation, Number* result) {
+    char output_file_path[512];
+
+    // OUTPUT FILE
+    printf("Enter the path to the output file: ");
+    fgets(output_file_path, sizeof(output_file_path), stdin);
+    output_file_path[strcspn(output_file_path, "\n")] = '\0';
+
+    // OPEN FILE
+    FILE* arq_output = fopen(output_file_path, "w");
+    file_verification(arq_output);
+
+    // STORES RESULT
+    save_result(arq_output, result);
+
+    // FREE RESOURCES
+    fclose(arq_output);
+    free_resources(operation);
+    free_number(result);
+}
